20e514e346032180fbfe12d3f4e9b9d442f55252
Taerrean <terrar1umand@gmail.com>
2024-06-27 12:59:53 +0300

    Add files via upload

---
+++ module 3 1.py
@@ -0,0 +1,26 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+    count_calls()
+    a = len(string)
+    b = string.upper()
+    c = string.lower()
+    return (a, b, c)
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if string.lower() in i.lower():
+            return True
+        else:
+            return False
+list = []
+a = int(input('Введите кол-во строк '))
+for i in range(a):
+    string = input('Введите строку ')
+    print(string_info(string))
+    list.append(string)
+line = input('Введите искомую строку ')
+print(is_contains(line, list))
+print(calls)
