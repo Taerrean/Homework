4ab859cd3ed581f241d44a0a8e5d8fe6261b6b34
Taerrean <terrar1umand@gmail.com>
2024-06-28 14:49:28 +0300

    Add files via upload

--- module 3 1.py
+++ module 3 1.py
@@ -9,12 +9,14 @@ def string_info(string):
     c = string.lower()
     return (a, b, c)
 def is_contains(string, list_to_search):
+    global is_in
     count_calls()
     for i in list_to_search:
-        if string.lower().strip() in i.lower().strip():
-            return True
-        else:
-            return False
+        is_in = string.lower().strip() in i.lower().strip()
+        if is_in == True:
+            break
+    print(is_in)
+    is_in = False
 list = []
 a = int(input('Введите кол-во строк '))
 for i in range(a):
