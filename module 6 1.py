81f8380eb5542a58b0d9373c062eadf076c5c313
Taerrean <terrar1umand@gmail.com>
2024-08-18 13:18:17 +0300

    Add files via upload

--- module 6 1.py
+++ module 6 1.py
@@ -3,19 +3,18 @@ class Animal:
     fed = False
     def __init__(self, name):
         self.name = name
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
 class Plant:
     edible = False
     def __init__(self, name):
         self.name = name
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
+    pass
 class Predator(Animal):
+    pass
     def eat(self, food):
         if food.edible:
             print(f'{self.name} съел {food.name}')
