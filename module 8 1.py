1943440adc4fd80d19fefdd67877b50315d8edf4
Taerrean <terrar1umand@gmail.com>
2024-08-23 21:09:44 +0300

    Add files via upload

--- module 8 1.py
+++ module 8 1.py
@@ -1,8 +1,8 @@
 def add_everything_up(a, b):
     try:
-        print(a + b)
+        return (a + b)
     except TypeError:
-        print(str(a) + str(b))
+        return (str(a) + str(b))
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
