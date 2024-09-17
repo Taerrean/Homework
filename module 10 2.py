22099a13873d183a7e5e3331ddfa8cacc69ca4c0
Taerrean <terrar1umand@gmail.com>
2024-09-17 16:36:11 +0300

    Add files via upload

--- module 10 2.py
+++ module 10 2.py
@@ -2,9 +2,9 @@ from threading import Thread
 from time import sleep
 class Knight(Thread):
     def __init__(self, name, power):
+        super().__init__()
         self.name = name
         self.power = power
-        super().__init__()
     def run(self):
         enemies = 100
         days = 0
@@ -17,5 +17,7 @@ class Knight(Thread):
         print(f'{self.name} одержал победу спустя {days} дней(дня)!')
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
+first_knight.start()
+second_knight.start()
 first_knight.join()
 second_knight.join()
