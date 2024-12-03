1915cb79141337339c692c91c02db0443ddc9138
Taerrean <terrar1umand@gmail.com>
2024-12-03 15:56:55 +0300

    Add files via upload

---
+++ module_17_2/app/user.py
@@ -0,0 +1,20 @@
+from app.backend.db import Base
+from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    __tablename__ = 'users'
+    uid = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship('Task', back_populates='user')
+
+
+from sqlalchemy.schema import CreateTable
+
+print(CreateTable(User.__table__))
