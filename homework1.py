3630f562d5381169fa0cfbb9e988f9e9071d80fa
Taerrean <terrar1umand@gmail.com>
2024-12-03 16:35:54 +0300

    Delete homework1.py

--- homework1.py
+++
@@ -1,6 +0,0 @@
-example = 'Террариум' # Код работает для всех строк с >= двумя символами
-print(example[0])
-print(example[-1])
-print(example[len(example)//2:])
-print(example[::-1])
-print(example[1::2])
