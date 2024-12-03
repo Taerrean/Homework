054ec046138061b6ebceb7f67b2fc2c9ecaceeeb
Taerrean <terrar1umand@gmail.com>
2024-12-03 16:36:14 +0300

    Delete homework2.py

--- homework2.py
+++
@@ -1,5 +0,0 @@
-Tasks_completed = 12
-Hours_spent = 1.5
-Course_name = 'Python'
-Average_time = Hours_spent/Tasks_completed
-print('Курс: ',Course_name,', всего задач: ',Tasks_completed,', затрачено часов: ', Hours_spent,', среднее время выполнения: ', Average_time,'.')
